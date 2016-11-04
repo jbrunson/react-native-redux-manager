fb34245f030512209965d3aa7cace3f1ce6958fc
Jeff Brunson <jeff.a.brunson@gmail.com>
2016-11-03 20:18:41 -0600

    add scenes

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Scene, Router } from 'react-native-router-flux';
 import LoginForm from './components/LoginForm';
+import EmployeeList from './components/EmployeeList';
 
 const RouterComponent = () => {
   return (
     <Router sceneStyle={{ paddingTop: 65 }}>
-      <Scene key="login" component={LoginForm} title="Please Login" />
+      <Scene key="auth">
+        <Scene key="login" component={LoginForm} title="Please Login" />
+      </Scene>
+
+      <Scene key="main">
+        <Scene key="employeeList" 
+               rightTitle="Add"
+               onRight={() => console.log("clicked")}
+               component={EmployeeList} 
+               title="Employees" />
+      </Scene>
     </Router>
   );
 };
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import { EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER } from './types';
 
 export const emailChanged = (text) => {
@@ -36,6 +37,8 @@ const loginUserSuccess = (dispatch, user) => {
     type: LOGIN_USER_SUCCESS, 
     payload: user
   });
+
+  Actions.main();
 };
 
 const loginUserFail = (dispatch) => {
